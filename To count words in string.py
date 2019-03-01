7e1d3848a241873e6f5523b9d3c2cca47139b884
kishorchouhan <devkishorbwr@gmail.com>
2019-03-01 14:40:07 +0530

    to count only words in string

---
+++ To count words in string.py
@@ -0,0 +1,26 @@
+# Write a procedure, count_words, which takes as input a string
+# and returns the number of words in the string. You may consider words
+# as strings of characters separated by spaces.
+
+def count_words(passage):
+    count = 0
+    abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    isword = True
+    for e in passage:
+        if e in abc:
+            isword = True
+        if isword:
+            if e not in abc:
+                count = count + 1
+                isword = False
+    return count
+
+
+passage =("The number of orderings of the 52 cards in a deck of cards "
+"is so great that if every one of the almost 7 billion people alive "
+"today dealt one ordering of the cards per second, it would take "
+"2.5 * 10**40 times the age of the universe to order the cards in every "
+"possible way.")
+print (count_words(passage))
+#>>>51
+
